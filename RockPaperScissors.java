9f202060bb884d31e88229058351b44a72369fe3
Rimantas Kazlauskas <konulv@gmail.com>
2022-08-11 13:31:54 +0100

    javadoc, formatted output a lil, did some cleaning, i hope

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -13,18 +13,20 @@ public class RockPaperScissors{
         int draw = 0;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("how many rounds would you like to play \"Rock, Paper, Scissors\"?");
-        int rounds = in.nextInt();
-        if (rounds < MIN_ROUNDS || rounds > MAX_ROUNDS){ //i.e. outide of allowed range
-            in.close();
-            System.exit(0);
-        }
         
         int userChoice;
         int PcChoice;
         int result;
+        int rounds = 0;
         boolean keep_playing = false;
         do{
+            System.out.println("how many rounds would you like to play \"Rock, Paper, Scissors\"?");
+            rounds = in.nextInt();
+            if (rounds < MIN_ROUNDS || rounds > MAX_ROUNDS){ //i.e. outide of allowed range
+                in.close();
+                System.exit(0);
+            }
+
             for(int i = 0; i < rounds; i++){
                 System.out.printf("beginning round %d\n", (i+1));
                 userChoice = user_choice(in);
@@ -35,7 +37,7 @@ public class RockPaperScissors{
                 if (result == 0){
                     System.out.println("a draw");
                     draw++;
-                } else if(result == -1 || result == 2){
+                } else if(result == -1 || result == 2){ //this logic taken form my other project, but it works.
                     System.out.println("won this round");
                     wins++;
                 } else { 
@@ -45,14 +47,18 @@ public class RockPaperScissors{
 
                 System.out.println(""); // there for formatting reasons
             }
-        
+
+            System.out.printf("you have:\nWon %d times\nLost %d times\nDrew %d times\n", wins, loss, draw);
+            showWinner(wins, loss);
+
             System.out.print("Would you like to keep playing? Yes/No: ");
             String kek = in.next().toLowerCase();
             keep_playing = kek.equals("yes");
 
         }while(keep_playing);
 
-        System.out.printf("you have:\nWon %d times\nLost %d times\nDrew %d times\n", wins, loss, draw);
+        System.out.println("thank you for playing");
+        in.close();
 
 
     }
@@ -60,7 +66,6 @@ public class RockPaperScissors{
     /**
      * Given the values, produces a random number within specified range (inclusive).
      * Might not work with negative values
-     * 
      * @param min - minimum value
      * @param max - maximum value
      * 
@@ -71,36 +76,63 @@ public class RockPaperScissors{
         return num;
     }
 
+    /**
+     * Taking user input for Rock/Paper/Scissors.
+     * Due to special logic, the input needs to be changed to
+     * a one in usable form.
+     * @param in - scanner for taking user input
+     * 
+     * @return int of correct value
+     */
     public static int user_choice(Scanner in){
         System.out.println("Please enter a number for your choice:");
         System.out.println("1: Rock\n2: Paper\n3: Scissors");
         int[] allowed_choices = {1,2,3};
         int[] converted_choice = {0,2,1};
         int answer = in.nextInt();
-        while(!in(allowed_choices, answer)){
-            System.out.print("The answer given is outside range, pSystmelease try again: ");
+        while(!in(answer, allowed_choices)){
+            System.out.print("The answer given is outside the range, please try again: ");
             answer = in.nextInt();
         }
 
         return converted_choice[answer-1];
     }
 
-
-    public static boolean in(Object[] obj, Object val){
-        for(int i = 0; i < obj.length; i++){
-            if(obj[i] == val){
+    /**
+     * This function should be a semi replacement for '1 in {1,2,3}'
+     * sort of expressions.
+     * @param val - value that one is looking for
+     * @param arr - array of values to be checked
+     * 
+     * @return boolean of whatever value exists in the array
+     */
+    public static boolean in(int val, int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == val){
                 return true;
             }
         }
         return false;
     }
-    public static boolean in(int[] obj, int val){
-        for(int i = 0; i < obj.length; i++){
-            if(obj[i] == val){
-                return true;
-            }
+
+    /**
+     * arguably this is unecessery, but i wanted a cleaner main function
+     * 
+     * @param win - number of wins the player has
+     * @param loss - number of times player lost
+     */
+    public static void showWinner(int win, int loss){
+        String winner;
+        if (win == loss){
+            winner = "No one";
+        }else if (win > loss){
+            winner = "Player";
+        }else{
+            winner = "Computer";
         }
-        return false;
+        System.out.println(winner + " has won this time");
+
+
     }
 
     
