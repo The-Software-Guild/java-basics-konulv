f8bde1fccfa452ee0bc6cbbd0ebe2a6c2000a736
Rimantas Kazlauskas <konulv@gmail.com>
2022-08-11 09:39:45 +0100

    added user choice function

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
-public class RockPaperScissors{    
-    public static void main(String[] args){
-        final int MAX_ROUNDS = 10;
-        final int MIN_ROUNDS = 1;
+public class RockPaperScissors{  
+    
+    static final int MAX_ROUNDS = 10;
+    static final int MIN_ROUNDS = 1;
         //there is a reason for the specific order bellow, makes logic cleaner
-        final String[] MOVES = {"rock", "scissors", "paper"};
-
-
+    static final String[] MOVES = {"rock", "scissors", "paper"};
+    public static void main(String[] args){
         int wins, loss, draw = 0; // for the player
         Scanner in = new Scanner(System.in);
 
@@ -17,6 +16,7 @@ public class RockPaperScissors{
             in.close();
             System.exit(0);
         }
+
 
     }
 
@@ -32,6 +32,38 @@ public class RockPaperScissors{
     public static int getNum(int min, int max){
         int num = (int)(Math.random() * ((max) - min + 1) + min);
         return num;
+    }
+
+    public static int user_choice(Scanner in){
+        System.out.println("Please enter a number for your choice:");
+        System.out.println("1: Rock\n2: Paper\n3: Scissors");
+        int[] allowed_choices = {1,2,3};
+        int[] converted_choice = {1,3,2};
+        int answer = in.nextInt();
+        while(!in(allowed_choices, answer)){
+            System.out.print("The answer given is outside range, please try again: ");
+            answer = in.nextInt();
+        }
+
+        return converted_choice[answer-1];
+    }
+
+
+    public static boolean in(Object[] obj, Object val){
+        for(int i = 0; i < obj.length; i++){
+            if(obj[i] == val){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean in(int[] obj, int val){
+        for(int i = 0; i < obj.length; i++){
+            if(obj[i] == val){
+                return true;
+            }
+        }
+        return false;
     }
 
     
